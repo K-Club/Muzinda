b60b5f768a47bdd3b37a7f7a14f9c818339a6e7a
unknown <kclubmuzie@gmail.com>
2018-11-14 18:15:38 +0200

    first commit

---
+++ app/src/main/java/zw/co/kclub/www/kclubwebapp/MainActivity.java
@@ -0,0 +1,38 @@
+package zw.co.kclub.www.kclubwebapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webViewId);
+
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://kclub.co.zw");
+    }
+}
